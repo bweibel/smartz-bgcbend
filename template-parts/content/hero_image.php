f5d6cecd313194469d782c047951536c50afc19e
Ben Weibel <weibel@gmail.com>
2020-05-03 22:07:27 -0700

    add wide window media query

---
+++ template-parts/content/hero_image.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template part for displaying a Hero Image
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+?>
+
+<section class="hero">
+		<?php the_post_thumbnail(); ?>
+</section><!-- .entry-header -->
