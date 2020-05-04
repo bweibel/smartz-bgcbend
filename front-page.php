9607d442e0ee0228e949e61f7ae1f158582760c8
Ben Weibel <weibel@gmail.com>
2020-05-03 22:07:05 -0700

    Activate homepage template

--- front-page.php
+++ front-page.php
@@ -12,23 +12,20 @@ namespace WP_Rig\WP_Rig;
 get_header();
 
 // Use grid layout if blog index is displayed.
-if ( is_home() ) {
-	wp_rig()->print_styles( 'wp-rig-content', 'wp-rig-front-page' );
-} else {
-	wp_rig()->print_styles( 'wp-rig-content' );
-}
+wp_rig()->print_styles( 'wp-rig-content', 'wp-rig-front-page' );
 
 ?>
+	<?php get_template_part('template-parts/content/hero_image'); ?>
 	<main id="primary" class="site-main">
+
 		<?php
 
 		while ( have_posts() ) {
 			the_post();
-
-			get_template_part( 'template-parts/content/entry', get_post_type() );
+			get_template_part( 'template-parts/content/entry_content');		
 		}
 
-		get_template_part( 'template-parts/content/pagination' );
+
 		?>
 	</main><!-- #primary -->
 <?php
