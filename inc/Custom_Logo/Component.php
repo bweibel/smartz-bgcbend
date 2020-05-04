7e7cd86000af1a5bd1a769eda952ae3a5d774cb2
Ben Weibel <weibel@gmail.com>
2020-05-03 22:33:48 -0700

    Adjust default logo size

--- inc/Custom_Logo/Component.php
+++ inc/Custom_Logo/Component.php
@@ -44,8 +44,8 @@ class Component implements Component_Interface {
 			apply_filters(
 				'wp_rig_custom_logo_args',
 				[
-					'height'      => 250,
-					'width'       => 250,
+					'height'      => 70,
+					'width'       => 120,
 					'flex-width'  => false,
 					'flex-height' => false,
 				]
