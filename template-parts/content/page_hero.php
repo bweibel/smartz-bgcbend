2350ce7a6cc2d67a55aba7bc25d1d681d2d2dfbc
Ben Weibel <weibel@gmail.com>
2020-05-10 16:22:42 -0700

    Add hero image template part

---
+++ template-parts/content/page_hero.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Template part for displaying a Hero Image
+ *
+ * @package wp_rig
+ */
+
+namespace WP_Rig\WP_Rig;
+
+$hero = get_field('hero');
+
+
+?>
+
+<section class="hero">
+	<img src="<?php echo esc_url( $hero['image']['url'] ); ?>" alt="<?php echo esc_attr( $hero['image']['alt'] ); ?>" />
+	<div class="hero-cta">
+		<?php echo $hero['call_to_action']; ?>
+	</div>
+</section><!-- .entry-header -->
