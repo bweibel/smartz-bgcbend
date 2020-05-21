6c64767539cb7342201b485da689eddee6e8be19
Ben Weibel <weibel@gmail.com>
2020-05-21 15:29:50 -0700

    Add footer contact info

--- template-parts/footer/info.php
+++ template-parts/footer/info.php
@@ -7,7 +7,36 @@
 
 namespace WP_Rig\WP_Rig;
 
+$theme = get_template_directory_uri();
 ?>
+
+<div class="info wrap-large">
+	<div class="logo">
+		<img src="<?php echo $theme;?>/assets/images/BGCB-Website-Logo-Stacked-White.svg" alt="">
+	</div>
+	<div class="connect">
+		<h3>Connect With Us</h3>
+			<div class="address">
+				<p><strong>Administration Office & Downtown Location: </strong><br>
+				500 NW Wall Street <br>
+				Bend, OR 97703 <br>
+				<strong>(541) 617-2877</strong>
+				</p>
+			</div>
+			<div class="address">
+				<p><strong>East Bend Location: </strong><br>
+				1707 Tempest Dr. <br>
+				Bend, OR 97703 <br>
+				<strong>(541) 617-2877</strong>
+				</p>
+			</div>
+	</div>
+	<div class="social">
+				<a href="#"><img src="<?php echo $theme;?>/assets/images/BGCB-Website-Icon-Social-Facebook.svg" alt="Facebook"></a>
+				<a href="#"><img src="<?php echo $theme;?>/assets/images/BGCB-Website-Icon-Social-Twitter.svg" alt="Twitter"></a>
+				<a href="#"><img src="<?php echo $theme;?>/assets/images/BGCB-Website-Icon-Social-YouTube.svg" alt="Youtube"></a>
+			</div>
+</div>
 
 <div class="site-info">
 
