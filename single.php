e6b20aa092737197818b7fb73ecf112d62ddf3e9
Ben Weibel <weibel.b@gmail.com>
2020-12-28 16:29:38 -0800

    Add meta data for blog posts

--- single.php
+++ single.php
@@ -13,12 +13,20 @@ get_header();
 
 wp_rig()->print_styles( 'wp-rig-content' );
 
+$colors = ['blue', 'green', 'orange', 'purple'];
+$color = $colors[rand(0, 3)];
+
 ?>
 	<main id="primary" class="site-main">
+	<header class="entry-header <?php echo $color ?>">
+			<h1 class="entry-title">Events & News</h1>
+		</header>
 		<?php
 
 		while ( have_posts() ) {
 			the_post();
+
+			// get_template_part( 'template-parts/content/entry', 'page' );
 
 			get_template_part( 'template-parts/content/entry', 'get_post_type()' );
 		}
