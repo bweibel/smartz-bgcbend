ef9941d8d08555f6daa357a05dd3c52867904ec0
Ben Weibel <weibel.b@gmail.com>
2020-12-28 16:29:50 -0800

    Add team member taxonomy filtering

--- template-parts/team-member/team_list.php
+++ template-parts/team-member/team_list.php
@@ -9,20 +9,31 @@ namespace WP_Rig\WP_Rig;
 use WP_Query;
 
 $theme = get_template_directory_uri();
+$teams = get_field('team_list_type');
+
 
 
 $args = array( 
   'post_type' => 'team_member',
   'orderby'     => 'date',
   'order'     => 'ASC',
-  'posts_per_page' => -1
+  'posts_per_page' => -1,
+  'tax_query' => array(
+      array(
+        'taxonomy' => 'team_category',
+        'field'    => 'slug',
+        'terms'    => $teams->slug
+      )
+  )
 );
+
 $the_query = new WP_Query( $args ); 
 
 ?>
 <section class="team-list wrap-large">
-	
 	<?php if ( $the_query->have_posts() ) : ?>
+
+ 
       <?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?>
         
         <?php get_template_part( 'template-parts/team-member/team_card' ); ?>
